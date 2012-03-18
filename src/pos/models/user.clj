08ac8c63f479730ef9c03009467e20e7ede4e4e3
zonpantli <zonpantli@gmail.com>
2012-03-18 21:04:52 +0200

    initial commit

---
+++ src/pos/models/user.clj
@@ -0,0 +1,1 @@
+(ns pos.models.user)
---
+++ src/pos/views/main.clj
@@ -0,0 +1,8 @@
+(ns pos.views.main
+  (:require [pos.views.common :as common])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/" []
+         (common/layout
+           [:div#content]))
