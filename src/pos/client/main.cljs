007a3b04672b3b144cd0f8a40983afc8f7e19ff7
zonpantli <zonpantli@gmail.com>
2012-03-21 22:27:00 +0200

    add scss and compass

--- src/pos/client/main.cljs
+++ src/pos/client/main.cljs
@@ -1,9 +1,11 @@
 (ns pos.client.main
   (:require [noir.cljs.client.watcher :as watcher]
             [clojure.browser.repl :as repl]
-            [crate.core :as crate])
+            [crate.core :as crate]
+            [fetch.remotes :as remotes])
   (:use [jayq.core :only [$ append]])
-  (:use-macros [crate.macros :only [defpartial]]))
+  (:use-macros [crate.macros :only [defpartial]])
+  (:require-macros [fetch.macros :as fm]))
 
 ;;************************************************
 ;; Dev stuff
@@ -22,5 +24,17 @@
   [:p.alert "CLJS is compiled and active... Time to build something!"])
 
 (append $content (up-and-running))
+
+
+;;;;;;;
+
+(defn adder [a b]
+  (fm/remote (adder a b) [result]
+             (js/alert result)))
+
+(defn get-user [id]
+  (fm/remote (get-user id) [{:keys [username age]}]
+             (js/alert (str "Name: " username ", Age: " age))))
+
 
 
--- src/pos/views/foo.clj
+++ src/pos/views/foo.clj
@@ -1,9 +1,51 @@
 (ns pos.views.foo
   (:require [pos.views.common :as common])
   (:use [noir.core :only [defpage defpartial]]
-        [hiccup.core :only [html]]))
+        [hiccup.core :only [html escape-html h]]
+        [hiccup.page-helpers]
+        [hiccup.form-helpers]
+        [noir.fetch.remotes]))
 
 
 (defpage [:get "/foo/:id"] {:keys [id s]}
   (str "You fooed " id " and strred " s))
+
+;; partials
+(defpartial page []
+  (html [:p "hola"])
+  (link-to {:class "btn"} "http://google.com" "Google"))
+
+(defpartial formel []
+  (form-to [:post "/login"]
+           (text-field "Username")
+           (password-field "Password")
+           (submit-button {:class "btn"} "Login")))
+
+(def items [{:name "Foo" :class "foo"}
+            {:name "Bar" :class "bar"}])
+
+(defpartial list-item [{:keys [name class]}]
+  [:li.item
+   [:h2 {:class class} name]])
+
+(defpartial item-list [items]
+  [:ul.items
+   (map list-item items)])
+
+(defpage "/hiccup" []
+  (common/layout
+   [:div#content
+    (page)
+    (formel)
+    (item-list items)
+    ]))
+
+;;;;; remote endpoints
+
+(defremote adder [& nums]
+           (apply + nums))
+
+(defremote get-user [id]
+           {:username "Miika"
+            :age 29})
 
