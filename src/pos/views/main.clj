0b28f4b23fce35223d47e65e92a4984f0f47f24d
zonpantli <zonpantli@gmail.com>
2012-03-23 02:30:13 +0200

    placeholders

--- src/pos/views/main.clj
+++ src/pos/views/main.clj
@@ -23,31 +23,29 @@
    ))
 
 (defpartial selects []
-  [:div#selects.row
-   [:div.span4.offset1
-    [:input#customer-dropdown]
-    [:div#customer-dropdown-select.btn-group
-     [:a {:class       "btn dropdown-toggle"
+  [:div#selects
+   [:div.row
+    [:div.span4.offset1
+     [:input#customer-dropdown {:placeholder "Customer name"}]
+     [:a {:id          "customer-dropdown-button"
+          :class       "btn dropdown-toggle"
           :data-toggle "dropdown"
           :href        "#"}
       [:span.caret]]
      [:ul.dropdown-menu
       [:li "foo"]
       [:li "bar"]]]]
-   [:div.span4.offset1
-    [:input#product-dropdown]
-    [:div.btn-group
-     [:a {:class       "btn dropdown-toggle"
-          :data-toggle "dropdown"
-          :href        "#"}
-      [:span.caret]]
-     [:ul.dropdown-menu
-      [:li "foo"]
-      [:li "bar"]]]]])
-
-(defpartial buttons []
-  [:div#editor
-   [:a.btn "Foo"]])
+   [:div.row
+    [:div.span4.offset1
+      [:input#item-dropdown {:placeholder "Product name"}]
+      [:a {:id          "item-dropdown-button"
+           :class       "btn dropdown-toggle"
+           :data-toggle "dropdown"
+           :href        "#"}
+       [:span.caret]]
+      [:ul.dropdown-menu
+       [:li "foo"]
+       [:li "bar"]]]]])
 
 
 (defpage "/" []
