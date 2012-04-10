91887a3745cb4b93dfe3161e21574df02565827e
zonpantli <zonpantli@gmail.com>
2012-04-11 01:07:07 +0300

    update items

--- src/pos/models/item.clj
+++ src/pos/models/item.clj
@@ -1,14 +1,32 @@
 (ns pos.models.item)
 
-(def fake-data [{:name "Cotton byxor"
-                 :id "1234567-456"
-                 :size ["M" "L"]
+(def fake-data [{:name  "Cotton byxor"
+                 :id    "1234567-456"
+                 :size  ["M" "L"]
                  :color ["red" "khaki" "blue"]
                  :price 120.99}
-                {:name "Cristal bag by Method"
-                 :id "3456789-234"
-                 :price 69.00}
-                {:name "Gap Gemini Shirt"
-                 :id "8445-32"
-                 :size ["S" "M" "XXL"]
-                 :price 230.00}])
+                {:name  "Don Linen pants"
+                 :id    "432123-532"
+                 :size  ["S" "M" "L"]
+                 :color ["green" "yellow"]
+                 :price 59.90}
+                {:name  "Cotton t-shirt Clark"
+                 :id    "262648-100"
+                 :size  ["S" "M" "L" "XL"]
+                 :color ["white" "black" "red"]
+                 :price 19.90}
+                {:name  "Burlington checked socks 5-pack"
+                 :id    "964682-145"
+                 :size  ["S" "M" "L" "XL"]
+                 :color ["black" "pink" "nude"]
+                 :price 24.95}
+                {:name  "Kangol hat Daddy O"
+                 :id    "875307-027"
+                 :size  ["S" "M" "L" "XL"]
+                 :color ["orange" "green" "navy"]
+                 :price 19.95}
+                {:name  "Monaco bow tie"
+                 :id    "391409-124"
+                 :color ["gray"]
+                 :price 19.95}
+                ])
